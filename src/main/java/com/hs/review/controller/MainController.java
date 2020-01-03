29a962bf385c3ecf30e3b2b1086c61b204ed19f2
hanseul <hsdeb11@gmail.com>
2020-01-03 19:57:23 +0900

    hs_jsp rendering(change to war)

---
+++ src/main/java/com/hs/review/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.hs.review.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/")
+	public String index() {
+		return "/jsp/index";
+	}
+}
